0693dc09c22fd779d482650de6d5756c38d11ed2
elainesb28 <belaine2828@gmail.com>
2026-08-29 21:50:01 -0300

    Refatorando os testes

--- functional_tests.py
+++ functional_tests.py
@@ -3,11 +3,24 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def test_can_start_a_list_and_retrieve_it_later(self): 
+    def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
+
+    def tearDown(self):
         self.browser.quit()
 
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouviu falar de uma nova aplicação online interessante para
+        # lista de tarefas. Ela vai conferir sua homepage
+        self.browser.get('http://localhost:8000')
+
+        # Ela nota que o título da página e o cabeçalho mencionam listas de
+        # tarefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Ela é convidada a inserir um item de tarefa imediatamente
+        # ...
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(warnings='ignore')
