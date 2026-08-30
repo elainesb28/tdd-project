3fd120ee89b96ece68364770083edc840a68a864
elainesb28 <belaine2828@gmail.com>
2026-08-29 22:11:51 -0300

    Ajustando o FT para testar criacao de itens

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,13 +17,35 @@ class NewVisitorTest(unittest.TestCase):
         # lista de tarefas. Ela vai conferir sua homepage
         self.browser.get('http://localhost:8000')
 
-        # Ela nota que o título da página e o cabeçalho mencionam listas de
-        # tarefas (to-do)
+        # Ela nota que o título da página e o cabeçalho mencionam listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente
-        # ...
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa de texto
+        inputbox.send_keys('Buy peacock feathers')
+
+        # Quando ela mecla enter, a página atualiza, e agora a página lista
+        # "1: Buy peacock feathers" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        )
+
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
